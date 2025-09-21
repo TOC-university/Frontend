602e8229902f08d89463247e2407bea0ed748526
pusida <pusida.bubby@gmail.com>
2025-09-21 15:54:22 +0700

    initial commit with React + Vite + Tailwind setup

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Frontend
+# TOC Frontend
+
+Frontend TOC **React + TypeScript + Vite + TailwindCSS**  
+
+# 1. Install dependencies
+npm install
+
+# 2. Run development server
+npm run dev
+
+# 3. go to localhost
+[localhost](http://localhost:5173/)
---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Main from "./pages/Main";
+
+export default function App() {
+  return (
+    <Router>
+      <div className="min-h-screen">
+        {/* Navigation */}
+        <nav className="flex gap-4 p-4 bg-gray-800 text-white">
+          <Link className="text-green-400 hover:text-green-200" to="/">Home</Link>
+          <Link className="text-green-400 hover:text-green-200" to="/about">About</Link>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
