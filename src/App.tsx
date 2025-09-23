89326cd629391dcb465bf255d69d4d6479f54ed4
CANO <pusita.anchan@gmail.com>
2025-09-23 22:35:08 +0700

    add navbar and scoll bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import HomePage from "./pages/HomePage";
+import Logo from "./assets/logo-nav.svg";
+import CatGithub from "./assets/cat-github.svg";
 
 export default function App() {
   return (
     <Router>
-      <div className="min-h-screen">
-        {/* <nav className="flex gap-4 p-4 bg-gray-800 text-white">
-          <Link className="text-green-400 hover:text-green-200" to="/">Home</Link>
-          <Link className="text-green-400 hover:text-green-200" to="/about">About</Link>
-        </nav> */}
+      <div className="h-screen flex flex-col ">
+        {/* Navbar */}
+        <nav className="flex justify-between items-center px-20 py-5 fixed bg-white/80 backdrop-blur-md z-50 opacity-99 w-full">
+          <div className="flex items-center space-x-2">
+            <img className="h-10 w-10" src={Logo} alt="universities search" />
+            <span className="font-bold text-purple-100 text-2xl">
+              Universities<span className="text-pink-100">Search</span>
+            </span>
+          </div>
+          <a
+            href="https://github.com/TOC-university"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex gap-2 justify-center items-center bg-purple-100 text-white px-5 py-2 rounded-full hover:bg-purple-200 transition"
+          >
+            <img className="h-6 w-6" src={CatGithub} alt="GitHub" />
+            GitHub
+          </a>
+        </nav>
 
         {/* Routes */}
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/Home" element={<HomePage />} />
-        </Routes>
+        <div className="flex-1 ">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/home" element={<HomePage />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
 }
-
