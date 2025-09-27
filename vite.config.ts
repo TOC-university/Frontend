cd7a670ca2a8a70f27257990a7e493aaf6825e64
Kerntarn <fluk032548@gmail.com>
2025-09-27 18:51:26 +0000

    Fix: allowed host one last time

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    allowedHosts: 'all'
+    allowedHosts: ['http://toc-university.member.ce-nacl.com', 'toc-university.member.ce-nacl.com']
   }
 })
