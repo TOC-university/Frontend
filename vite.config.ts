e7f2290df7992189f38bc0fd1701cdf1c01dc751
Kerntarn <fluk032548@gmail.com>
2025-09-27 18:39:39 +0000

    Fix: allowed all host

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    allowedHosts: [
-      'toc-university.member.ce-nacl.com',
-    ]
+    allowedHosts: 'all'
   }
 })
