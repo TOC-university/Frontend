709b500681cb1112fcb36922b01fc14b7fd3e695
Siasa <siasa18037@gmail.com>
2025-10-02 14:46:47 +0700

    Edit Campuses and Faculties table

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -78,6 +78,8 @@ export default function DetailPage() {
     }
   }, [university]);
 
+  console.log(universityDetail)
+
   return (
     <div className="h-screen bg-gradient-to-b from-white to-pink-50 flex flex-col overflow-y-auto scrollbar-custom pt-20 relative">
       <img src={BackWorld} alt="background" className="absolute bottom-0 object-cover z-0" />
@@ -113,26 +115,28 @@ export default function DetailPage() {
           </div>
         )}
 
-        {/* Success (show detail only if no error and not loading) */}
         {!loading && !error && universityDetail && (
           <>
             {/* detail */}
-            <div className="w-full flex flex-col md:flex-row gap-6 items-center md:items-start mb-4">
-              <div className="flex-shrink-0">
-                {universityDetail?.logo_url ? (
-                  <img
-                    src={universityDetail.logo_url}
-                    alt={`${universityDetail?.name} logo`}
-                    className="w-[250px] h-[250px] object-contain rounded-3xl border-2 border-pink-50 bg-white p-2"
-                  />
-                ) : (
-                  <div className="w-[250px] h-[250px] flex items-center justify-center rounded-3xl border-2 border-pink-50 bg-white">
-                    No Logo
-                  </div>
-                )}
-              </div>
+            <div className="w-full flex flex-col md:flex-row gap-6 items-center md:items-stretch mb-4">
+              
+              {/* {universityDetail?.logo_url &&( */}
+                <div className="flex-shrink-0 h-full">
+                  {universityDetail?.logo_url ? (
+                    <img
+                      src={universityDetail.logo_url}
+                      alt={`${universityDetail?.name} logo`}
+                      className="w-[250px] h-[250px] object-contain rounded-3xl border-2 border-pink-50 bg-white p-2"
+                    />
+                  ) : (
+                    <div className="w-[250px] h-[250px] flex items-center justify-center rounded-3xl border-2 border-pink-50 bg-white">
+                      No Logo
+                    </div>
+                  )}
+                </div>
+              {/* )} */}
 
-              <div className="w-full flex flex-col justify-between text-left rounded-3xl border-2 border-pink-50 bg-white p-5 space-y-2">
+              <div className="w-full flex flex-col justify-between text-left rounded-3xl border-2 border-pink-50 bg-white p-5 space-y-2 h-full">
                 <p className="text-purple-100 text-xl">
                   <span className="text-black me-4">Name :</span> {universityDetail?.name}
                 </p>
@@ -141,9 +145,6 @@ export default function DetailPage() {
                 </p>
                 <p className="text-purple-100 text-xl">
                   <span className="text-black me-4">Location :</span> {universityDetail?.location}
-                </p>
-                <p className="text-purple-100 text-xl">
-                  <span className="text-black me-4">President :</span> {universityDetail?.president}
                 </p>
                 <p className="text-purple-100 text-xl">
                   <span className="text-black me-4">Established :</span> {universityDetail?.established}
@@ -157,25 +158,30 @@ export default function DetailPage() {
               </div>
             </div>
 
+
             {/* Campuses Table */}
-            <div className="w-full rounded-3xl overflow-hidden border-2 border-pink-50 mb-4">
-              <table className="w-full text-left">
-                <thead className="bg-pink-50">
-                  <tr>
-                    <th className="px-6 py-4 ">Campuses</th>
-                  </tr>
-                </thead>
-                <tbody className="bg-white">
-                  {universityDetail?.campus.map((campus, idx) => (
-                    <tr key={idx} className="border-t-1 border-pink-50">
-                      <td className="px-6 py-4 ">{campus}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+            {universityDetail?.campus.length != 0 && (
+                <div className="w-full rounded-3xl overflow-hidden border-2 border-pink-50 mb-4">
+                  <table className="w-full text-left">
+                    <thead className="bg-pink-50">
+                      <tr>
+                        <th className="px-6 py-4 ">Campuses</th>
+                      </tr>
+                    </thead>
+                    <tbody className="bg-white">
+                      {universityDetail?.campus.map((campus, idx) => (
+                        <tr key={idx} className="border-t-1 border-pink-50">
+                          <td className="px-6 py-4 ">{campus}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+            )}
+            
 
             {/* Faculties Table */}
+            {universityDetail?.faculties.length != 0 && (
             <div className="w-full rounded-3xl overflow-hidden border-2 border-pink-50 mb-4">
               <table className="w-full text-left">
                 <thead className="bg-pink-50">
@@ -192,6 +198,8 @@ export default function DetailPage() {
                 </tbody>
               </table>
             </div>
+            )}
+
           </>
         )}
       </div>
